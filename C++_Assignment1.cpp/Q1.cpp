48d25c0e97b690cbc804e7b7a767c8925992b22b
Aditya Kumar <160321061+Aditya08kumar@users.noreply.github.com>
2024-02-17 17:04:46 +0530

    Add files via upload

---
+++ C++_Assignment1.cpp/Q1.cpp
@@ -0,0 +1,9 @@
+//By using  \n  or   endl 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Physics"<<endl<<"Wallah";
+    return 0;
+}
---
+++ C++_Assignment1.cpp/Q5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r = 10, A, pi = 3.1415;
+
+    float circumference = 2 * pi * r;
+
+    cout << "Circumference of the circle is : " << circumference;
+    return 0;
+}
